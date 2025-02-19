7f6760e5372dcad0924e2c8556c73ddad4a7e923
faisalsajid <faisalsajid535@gmail.com>
2025-02-19 16:04:20 +0500

    add functionalities

--- patient_screen.py
+++ patient_screen.py
@@ -1,8 +1,3 @@
-patient_name=input("please enter patient name: ")
-patient_contact=int(input("please enter your contact number: "))
-patient_address=input("please enter your address: ")
-patient_age=int(input("please enter patient age: "))
-
 print("Thank You")
 print("Please select one from below")
 print("1.Book an appoipent")
@@ -11,11 +6,41 @@ print("3.Lab")
 print("4.Shutdown")
 choice=int(input("which one you prefer: "))
 
+def doc__app (self):
+
+    print("Available doctors")
+    print("1. Afzal(skin specalist)")
+    print("2. Muawar(bone specalist)")
+    print("1. Hasseb (ear specalist)")
+    print("1. Adnan (eye specalist)")
+    choice=int(input("Enter your choice: "))
+
+    patient_name=input("please enter patient name: ")
+    patient_contact=int(input("please enter your contact number: "))
+    patient_address=input("please enter your address: ")
+    patient_age=int(input("please enter patient age: "))
+
+def medical_store(self):
+
+    customer_name=input("please enter your name: ")
+    address=input("Please enter your address: ")
+    order=True
+
+    if order==True :
+        name=input("please enter the name of mediciene: ")
+
+def lab(self):
+
+    patient_name=input("please enter patient name: ")
+    patient_contact=int(input("please enter your contact number: "))
+    patient_address=input("please enter your address: ")
+    patient_age=int(input("please enter patient age: "))
+
 if choice==1 :
-
+    doc__app(None)    
 elif choice==2:
-
+    medical_store(None)
 elif choice==3 :
-
+    lab(None)
 elif choice==4 :
-    break
+    exit()
