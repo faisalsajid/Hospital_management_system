a1350be1a74416da9883036b62fbb86912d65c0a
faisalsajid <faisalsajid535@gmail.com>
2025-02-19 15:37:20 +0500

    adding patient screen

---
+++ patient_screen.py
@@ -0,0 +1,21 @@
+patient_name=input("please enter patient name: ")
+patient_contact=int(input("please enter your contact number: "))
+patient_address=input("please enter your address: ")
+patient_age=int(input("please enter patient age: "))
+
+print("Thank You")
+print("Please select one from below")
+print("1.Book an appoipent")
+print("2.Medical store")
+print("3.Lab")
+print("4.Shutdown")
+choice=int(input("which one you prefer: "))
+
+if choice==1 :
+
+elif choice==2:
+
+elif choice==3 :
+
+elif choice==4 :
+    break
