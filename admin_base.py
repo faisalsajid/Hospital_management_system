881a98ec4963301ca99f08b5e17bcac46965e28a
faisalsajid <faisalsajid535@gmail.com>
2025-02-25 13:13:10 +0500

    add functionallity of inserting patient_data

--- admin_base.py
+++ admin_base.py
@@ -1,31 +1,56 @@
+import mysql.connector
+
+conn = mysql.connector.connect(host='localhost', user='root', password='SU92-BSCSM-F23-495', database='Hospital_management_system')
+
+if conn.is_connected():
+    print("Connection is established")
+
+
 id='0'
 password='0'
-no_of_bed=500
 
-def log_in (self,id,password):
+
+def add_new_patient():
+        patient_name=input('enter patient name: ')
+        patient_address=input('enter patient address: ')
+        patient_contact=input('enter patient contact: ')
+        patient_age=int(input('enter patient age: '))
+
+        cursor=conn.cursor()
+        insert_in_table="""insert into patient_info(name,address,contact,age) 
+            values (%s,%s,%s,%s)"""
+        data=(patient_name,patient_address,patient_contact,patient_age)
+        
+        try:
+            cursor.execute(insert_in_table, data)
+            conn.commit()
+            print("Data inserted successfully")
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+        cursor.close()
+
+
+def search():
+    patient_name=input('enter patient Name: ')
+    
+
+def log_in (id,password):
     login=input('insert your log in id: ')
     pin=input('enter your log in passowrd: ')
+    
     if login==id and pin==password:
         print('log in complete')
         print('1.add new patient')
-        print('2.see available no of bed')
-        print('3.see doctor list')
-        print('4.search patient')
+        print('2.see doctor list')
+        print('3.search patient')
         choice=int(input())
-            
-log_in(None,id,password)
+        if choice==1:
+            add_new_patient()
+        elif choice==3:
+            search()
+        else:
+            print("error")
 
-def add_new_patient(self):
-    patient_name=input('enter patient name: ')
-    patient_address=input('enter patient address: ')
-    patient_contact=input('enter patient contact: ')
-    patient_age=int(input('enter patient age: '))
+log_in(id,password)
 
-def search(self):
-    id=int(input('enter patient id: '))
-
-import mysql.connector
-
-conn=mysql.connector.connect(host='localhost', password='SU92-BSCSM-F23-495', username='root')
-if conn.is_connected():
-    print("connection is established")
+conn.close()
